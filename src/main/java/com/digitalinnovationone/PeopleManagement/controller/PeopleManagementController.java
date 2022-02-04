19f00602677ccddba7c7db952f9922d02c8f88f8
Rai Rafael <rairafaelss@gmail.com>
2022-02-04 01:30:57 -0300

    Created some packages

---
+++ src/main/java/com/digitalinnovationone/PeopleManagement/controller/PeopleManagementController.java
@@ -0,0 +1,18 @@
+package com.digitalinnovationone.PeopleManagement.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PeopleManagementController {
+
+
+}
