07623a95f3e40d304065e88ae2ce1c585557c23f
Rai Rafael <rairafaelss@gmail.com>
2022-02-04 01:19:58 -0300

    Made initial settings

---
+++ src/test/java/com/digitalinnovationone/PeopleManagement/PeopleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.PeopleManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
